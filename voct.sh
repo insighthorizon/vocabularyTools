7c1bf019ae7b1facf81390cd626845be297bd9be
insighthorizon <majiricek@seznam.cz>
2018-09-22 15:06:18 +0200

    Initial Commit

---
+++ voct.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# A tool for training vocabulary
+# Script takes .txt file containing vocabulary as argument
+# Vocabulary must contain lines in format:n
+# 'word/phrase in one language'-'that in other language'
+
