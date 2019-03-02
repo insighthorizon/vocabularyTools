18b5fc1b608abd0f7569293ad114edd0434bea11
prasopes <majiricek@seznam.cz>
2019-03-02 12:05:21 +0100

    Implemented utility for checking if list of items is alrady contained in the vocabulary

---
+++ checker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# A tool to quickly search if a list of items is already in the vocabulary
+# run with one argument: path to the list text file
+LIST=`sed '/^$/d' $1`
+NLINES=`echo "$LIST" | wc -l`
+NEW_ITEMS=''
+OLD_ITEMS=''
+for i in $(seq 1 $NLINES); do
+    ITEM=`echo "$LIST" | sed -n "$i"p`
+    ITEM_INFO=`grep -nr --include="voc*\.txt" "$ITEM"`
+    if [ "$ITEM_INFO" = '' ]; then
+	NEW_ITEMS+="$ITEM"
+	NEW_ITEMS+=$'\n'
+    else
+	OLD_ITEMS+="$ITEM_INFO"
+	OLD_ITEMS+=$'\n'
+    fi
+done
+
+echo "========= OLD ITEMS: ==========="
+echo "$OLD_ITEMS"
+echo "========= NEW ITEMS: ==========="
+echo "$NEW_ITEMS"
+
+echo "========= OLD ITEMS: ===========" >> $1
+echo "$OLD_ITEMS" >> $1
+echo "========= NEW ITEMS: ===========" >> $1
+echo "$NEW_ITEMS" >> $1
+
+exit 0
+
